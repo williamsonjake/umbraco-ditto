1c1a763046916c86253380b347e7fce44fb5640e
jake williamson <jake.williamson@gibedigital.com>
2015-01-21 11:02:21 +0000

    updated the umbraco version, updated the 'Activator' to use 'DependencyResolver' in PublishedContentExtensions to sort out dependency injection

--- src/Our.Umbraco.Ditto/Extensions/PublishedContentExtensions.cs
+++ src/Our.Umbraco.Ditto/Extensions/PublishedContentExtensions.cs
@@ -1,248 +1,250 @@
-﻿namespace Our.Umbraco.Ditto
+﻿using System.Web.Mvc;
+
+namespace Our.Umbraco.Ditto
 {
-    using System;
-    using System.Collections.Generic;
-    using System.ComponentModel;
-    using System.Linq;
-    using System.Reflection;
+	using System;
+	using System.Collections.Generic;
+	using System.ComponentModel;
+	using System.Linq;
+	using System.Reflection;
 
-    using global::Umbraco.Core;
-    using global::Umbraco.Core.Models;
-    using global::Umbraco.Web;
+	using global::Umbraco.Core;
+	using global::Umbraco.Core.Models;
+	using global::Umbraco.Web;
 
-    /// <summary>
-    /// Encapsulates extension methods for <see cref="IPublishedContent"/>.
-    /// </summary>
-    public static class PublishedContentExtensions
-    {
-        /// <summary>
-        /// Returns the given instance of <see cref="IPublishedContent"/> as the specified type.
-        /// </summary>
-        /// <param name="content">
-        /// The <see cref="IPublishedContent"/> to convert.
-        /// </param>
-        /// <param name="convertingType">
-        /// The <see cref="Action{ConvertingTypeEventArgs}"/> to fire when converting.
-        /// </param>
-        /// <param name="convertedType">
-        /// The <see cref="Action{ConvertedTypeEventArgs}"/> to fire when converted.
-        /// </param>
-        /// <typeparam name="T">
-        /// The <see cref="Type"/> of items to return.
-        /// </typeparam>
-        /// <returns>
-        /// The resolved <see cref="T"/>.
-        /// </returns>
-        public static T As<T>(
-            this IPublishedContent content,
-            Action<ConvertingTypeEventArgs> convertingType = null,
-            Action<ConvertedTypeEventArgs> convertedType = null)
-            where T : class
-        {
-            return content.As(typeof(T), convertingType, convertedType) as T;
-        }
+	/// <summary>
+	/// Encapsulates extension methods for <see cref="IPublishedContent"/>.
+	/// </summary>
+	public static class PublishedContentExtensions
+	{
+		/// <summary>
+		/// Returns the given instance of <see cref="IPublishedContent"/> as the specified type.
+		/// </summary>
+		/// <param name="content">
+		/// The <see cref="IPublishedContent"/> to convert.
+		/// </param>
+		/// <param name="convertingType">
+		/// The <see cref="Action{ConvertingTypeEventArgs}"/> to fire when converting.
+		/// </param>
+		/// <param name="convertedType">
+		/// The <see cref="Action{ConvertedTypeEventArgs}"/> to fire when converted.
+		/// </param>
+		/// <typeparam name="T">
+		/// The <see cref="Type"/> of items to return.
+		/// </typeparam>
+		/// <returns>
+		/// The resolved <see cref="T"/>.
+		/// </returns>
+		public static T As<T>(
+		this IPublishedContent content,
+		Action<ConvertingTypeEventArgs> convertingType = null,
+		Action<ConvertedTypeEventArgs> convertedType = null)
+		where T : class
+		{
+			return content.As(typeof(T), convertingType, convertedType) as T;
+		}
 
-        /// <summary>
-        /// Gets a collection of the given type from the given <see cref="IEnumerable{IPublishedContent}"/>.
-        /// </summary>
-        /// <param name="items">
-        /// The <see cref="IEnumerable{IPublishedContent}"/> to convert.
-        /// </param>
-        /// <param name="documentTypeAlias">
-        /// The document type alias.
-        /// </param>
-        /// <param name="convertingType">
-        /// The <see cref="Action{ConvertingTypeEventArgs}"/> to fire when converting.
-        /// </param>
-        /// <param name="convertedType">
-        /// The <see cref="Action{ConvertedTypeEventArgs}"/> to fire when converted.
-        /// </param>
-        /// <typeparam name="T">
-        /// The <see cref="Type"/> of items to return.
-        /// </typeparam>
-        /// <returns>
-        /// The resolved <see cref="IEnumerable{T}"/>.
-        /// </returns>
-        public static IEnumerable<T> As<T>(
-            this IEnumerable<IPublishedContent> items,
-            string documentTypeAlias = null,
-            Action<ConvertingTypeEventArgs> convertingType = null,
-            Action<ConvertedTypeEventArgs> convertedType = null)
-            where T : class
-        {
-            using (DisposableTimer.DebugDuration<IEnumerable<T>>(string.Format("IEnumerable As ({0})", documentTypeAlias)))
-            {
-                if (string.IsNullOrWhiteSpace(documentTypeAlias))
-                {
-                    return items.Select(x => x.As<T>(convertingType, convertedType));
-                }
+		/// <summary>
+		/// Gets a collection of the given type from the given <see cref="IEnumerable{IPublishedContent}"/>.
+		/// </summary>
+		/// <param name="items">
+		/// The <see cref="IEnumerable{IPublishedContent}"/> to convert.
+		/// </param>
+		/// <param name="documentTypeAlias">
+		/// The document type alias.
+		/// </param>
+		/// <param name="convertingType">
+		/// The <see cref="Action{ConvertingTypeEventArgs}"/> to fire when converting.
+		/// </param>
+		/// <param name="convertedType">
+		/// The <see cref="Action{ConvertedTypeEventArgs}"/> to fire when converted.
+		/// </param>
+		/// <typeparam name="T">
+		/// The <see cref="Type"/> of items to return.
+		/// </typeparam>
+		/// <returns>
+		/// The resolved <see cref="IEnumerable{T}"/>.
+		/// </returns>
+		public static IEnumerable<T> As<T>(
+		this IEnumerable<IPublishedContent> items,
+		string documentTypeAlias = null,
+		Action<ConvertingTypeEventArgs> convertingType = null,
+		Action<ConvertedTypeEventArgs> convertedType = null)
+		where T : class
+		{
+			using (DisposableTimer.DebugDuration<IEnumerable<T>>(string.Format("IEnumerable As ({0})", documentTypeAlias)))
+			{
+				if (string.IsNullOrWhiteSpace(documentTypeAlias))
+				{
+					return items.Select(x => x.As<T>(convertingType, convertedType));
+				}
 
-                return items
-                    .Where(x => documentTypeAlias.InvariantEquals(x.DocumentTypeAlias))
-                    .Select(x => x.As<T>(convertingType, convertedType));
-            }
-        }
+				return items
+				.Where(x => documentTypeAlias.InvariantEquals(x.DocumentTypeAlias))
+				.Select(x => x.As<T>(convertingType, convertedType));
+			}
+		}
 
-        /// <summary>
-        /// Returns an object representing the given <see cref="Type"/>.
-        /// </summary>
-        /// <param name="content">
-        /// The <see cref="IPublishedContent"/> to convert.
-        /// </param>
-        /// <param name="type">
-        /// The <see cref="Type"/> of items to return.
-        /// </param>
-        /// <param name="convertingType">
-        /// The <see cref="Action{ConvertingTypeEventArgs}"/> to fire when converting.
-        /// </param>
-        /// <param name="convertedType">
-        /// The <see cref="Action{ConvertedTypeEventArgs}"/> to fire when converted.
-        /// </param>
-        /// <returns>
-        /// The converted <see cref="Object"/> as the given type.
-        /// </returns>
-        /// <exception cref="InvalidOperationException">
-        /// Thrown if the given type has invalid constructors.
-        /// </exception>
-        internal static object As(
-            this IPublishedContent content,
-            Type type,
-            Action<ConvertingTypeEventArgs> convertingType = null,
-            Action<ConvertedTypeEventArgs> convertedType = null)
-        {
-            object instance = null;
+		/// <summary>
+		/// Returns an object representing the given <see cref="Type"/>.
+		/// </summary>
+		/// <param name="content">
+		/// The <see cref="IPublishedContent"/> to convert.
+		/// </param>
+		/// <param name="type">
+		/// The <see cref="Type"/> of items to return.
+		/// </param>
+		/// <param name="convertingType">
+		/// The <see cref="Action{ConvertingTypeEventArgs}"/> to fire when converting.
+		/// </param>
+		/// <param name="convertedType">
+		/// The <see cref="Action{ConvertedTypeEventArgs}"/> to fire when converted.
+		/// </param>
+		/// <returns>
+		/// The converted <see cref="Object"/> as the given type.
+		/// </returns>
+		/// <exception cref="InvalidOperationException">
+		/// Thrown if the given type has invalid constructors.
+		/// </exception>
+		internal static object As(
+		this IPublishedContent content,
+		Type type,
+		Action<ConvertingTypeEventArgs> convertingType = null,
+		Action<ConvertedTypeEventArgs> convertedType = null)
+		{
+			object instance = null;
 
-            if (content == null)
-            {
-                return instance;
-            }
+			if (content == null)
+			{
+				return instance;
+			}
 
-            using (DisposableTimer.DebugDuration(type, string.Format("IPublishedContent As ({0})", content.DocumentTypeAlias), "Complete"))
-            {
-                var constructor = type.GetConstructors()
-                    .OrderBy(x => x.GetParameters().Length)
-                    .First();
-                var constructorParams = constructor.GetParameters();
+			using (DisposableTimer.DebugDuration(type, string.Format("IPublishedContent As ({0})", content.DocumentTypeAlias), "Complete"))
+			{
+				var constructor = type.GetConstructors()
+				.OrderBy(x => x.GetParameters().Length)
+				.First();
+				var constructorParams = constructor.GetParameters();
 
-                var args1 = new ConvertingTypeEventArgs { Content = content };
+				var args1 = new ConvertingTypeEventArgs { Content = content };
 
-                EventHandlers.CallConvertingTypeHandler(args1);
+				EventHandlers.CallConvertingTypeHandler(args1);
 
-                if (!args1.Cancel && convertingType != null)
-                    convertingType(args1);
+				if (!args1.Cancel && convertingType != null)
+					convertingType(args1);
 
-                if (args1.Cancel)
-                    return instance;
+				if (args1.Cancel)
+					return instance;
 
-                if (constructorParams.Length == 0)
-                {
-                    instance = Activator.CreateInstance(type);
-                }
-                else if (constructorParams.Length == 1 & constructorParams[0].ParameterType == typeof(IPublishedContent))
-                {
-                    instance = Activator.CreateInstance(type, content);
-                }
-                else
-                {
-                    throw new InvalidOperationException("Type {0} has invalid constructor parameters");
-                }
+				if (constructorParams.Length == 0)
+				{
+					instance = Activator.CreateInstance(type);
+				}
+				else if (constructorParams.Length == 1 & constructorParams[0].ParameterType == typeof(IPublishedContent))
+				{
+					instance = Activator.CreateInstance(type, content);
+				}
+				else
+				{
+					throw new InvalidOperationException("Type {0} has invalid constructor parameters");
+				}
 
-                var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
-                var contentType = content.GetType();
+				var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+				var contentType = content.GetType();
 
-                foreach (var propertyInfo in properties.Where(x => x.CanWrite))
-                {
-                    using (DisposableTimer.DebugDuration(type, string.Format("ForEach Property ({1} {0})", propertyInfo.Name, content.Id), "Complete"))
-                    {
-                        // Check for the ignore attribute.
-                        var ignoreAttr = propertyInfo.GetCustomAttribute<DittoIgnoreAttribute>();
-                        if (ignoreAttr != null)
-                        {
-                            continue;
-                        }
+				foreach (var propertyInfo in properties.Where(x => x.CanWrite))
+				{
+					using (DisposableTimer.DebugDuration(type, string.Format("ForEach Property ({1} {0})", propertyInfo.Name, content.Id), "Complete"))
+					{
+						// Check for the ignore attribute.
+						var ignoreAttr = propertyInfo.GetCustomAttribute<DittoIgnoreAttribute>();
+						if (ignoreAttr != null)
+						{
+							continue;
+						}
 
-                        var umbracoPropertyName = propertyInfo.Name;
-                        var altUmbracoPropertyName = string.Empty;
-                        var recursive = false;
-                        object defaultValue = null;
+						var umbracoPropertyName = propertyInfo.Name;
+						var altUmbracoPropertyName = string.Empty;
+						var recursive = false;
+						object defaultValue = null;
 
-                        var umbracoPropertyAttr = propertyInfo.GetCustomAttribute<UmbracoPropertyAttribute>();
-                        if (umbracoPropertyAttr != null)
-                        {
-                            umbracoPropertyName = umbracoPropertyAttr.PropertyName;
-                            altUmbracoPropertyName = umbracoPropertyAttr.AltPropertyName;
-                            recursive = umbracoPropertyAttr.Recursive;
-                            defaultValue = umbracoPropertyAttr.DefaultValue;
-                        }
+						var umbracoPropertyAttr = propertyInfo.GetCustomAttribute<UmbracoPropertyAttribute>();
+						if (umbracoPropertyAttr != null)
+						{
+							umbracoPropertyName = umbracoPropertyAttr.PropertyName;
+							altUmbracoPropertyName = umbracoPropertyAttr.AltPropertyName;
+							recursive = umbracoPropertyAttr.Recursive;
+							defaultValue = umbracoPropertyAttr.DefaultValue;
+						}
 
-                        // Try fetching the value.
-                        var contentProperty = contentType.GetProperty(umbracoPropertyName);
-                        object propertyValue = contentProperty != null
-                                                ? contentProperty.GetValue(content, null)
-                                                : content.GetPropertyValue(umbracoPropertyName, recursive);
+						// Try fetching the value.
+						var contentProperty = contentType.GetProperty(umbracoPropertyName);
+						object propertyValue = contentProperty != null
+						? contentProperty.GetValue(content, null)
+						: content.GetPropertyValue(umbracoPropertyName, recursive);
 
-                        // Try fetching the alt value.
-                        if (propertyValue == null && !string.IsNullOrWhiteSpace(altUmbracoPropertyName))
-                        {
-                            contentProperty = contentType.GetProperty(altUmbracoPropertyName);
-                            propertyValue = contentProperty != null
-                                                ? contentProperty.GetValue(content, null)
-                                                : content.GetPropertyValue(altUmbracoPropertyName, recursive);
-                        }
+						// Try fetching the alt value.
+						if (propertyValue == null && !string.IsNullOrWhiteSpace(altUmbracoPropertyName))
+						{
+							contentProperty = contentType.GetProperty(altUmbracoPropertyName);
+							propertyValue = contentProperty != null
+							? contentProperty.GetValue(content, null)
+							: content.GetPropertyValue(altUmbracoPropertyName, recursive);
+						}
 
-                        // Try setting the default value.
-                        if (propertyValue == null && defaultValue != null)
-                        {
-                            propertyValue = defaultValue;
-                        }
+						// Try setting the default value.
+						if (propertyValue == null && defaultValue != null)
+						{
+							propertyValue = defaultValue;
+						}
 
-                        // Process the value.
-                        if (propertyValue != null)
-                        {
-                            if (propertyInfo.PropertyType.IsInstanceOfType(propertyValue))
-                            {
-                                propertyInfo.SetValue(instance, propertyValue, null);
-                            }
-                            else
-                            {
-                                using (DisposableTimer.DebugDuration(type, string.Format("TypeConverter ({0}, {1})", content.Id, propertyInfo.Name), "Complete"))
-                                {
-                                    var converterAttr = propertyInfo.GetCustomAttribute<TypeConverterAttribute>();
-                                    if (converterAttr != null)
-                                    {
-                                        var converter = Activator.CreateInstance(Type.GetType(converterAttr.ConverterTypeName)) as TypeConverter;
-                                        propertyInfo.SetValue(instance, converter.ConvertFrom(propertyValue), null);
-                                    }
-                                    else
-                                    {
-                                        var convert = propertyValue.TryConvertTo(propertyInfo.PropertyType);
-                                        if (convert.Success)
-                                        {
-                                            propertyInfo.SetValue(instance, convert.Result, null);
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+						// Process the value.
+						if (propertyValue != null)
+						{
+							if (propertyInfo.PropertyType.IsInstanceOfType(propertyValue))
+							{
+								propertyInfo.SetValue(instance, propertyValue, null);
+							}
+							else
+							{
+								using (DisposableTimer.DebugDuration(type, string.Format("TypeConverter ({0}, {1})", content.Id, propertyInfo.Name), "Complete"))
+								{
+									var converterAttr = propertyInfo.GetCustomAttribute<TypeConverterAttribute>();
+									if (converterAttr != null)
+									{
+										var converter = DependencyResolver.Current.GetService(Type.GetType(converterAttr.ConverterTypeName)) as TypeConverter;
+										propertyInfo.SetValue(instance, converter.ConvertFrom(propertyValue), null);
+									}
+									else
+									{
+										var convert = propertyValue.TryConvertTo(propertyInfo.PropertyType);
+										if (convert.Success)
+										{
+											propertyInfo.SetValue(instance, convert.Result, null);
+										}
+									}
+								}
+							}
+						}
+					}
+				}
 
-                var args2 = new ConvertedTypeEventArgs
-                {
-                    Content = content,
-                    Converted = instance,
-                    ConvertedType = type
-                };
+				var args2 = new ConvertedTypeEventArgs
+				{
+					Content = content,
+					Converted = instance,
+					ConvertedType = type
+				};
 
-                if (convertedType != null)
-                {
-                    convertedType(args2);
-                }
+				if (convertedType != null)
+				{
+					convertedType(args2);
+				}
 
-                EventHandlers.CallConvertedTypeHandler(args2);
+				EventHandlers.CallConvertedTypeHandler(args2);
 
-                return args2.Converted;
-            }
-        }
-    }
+				return args2.Converted;
+			}
+		}
+	}
 }
